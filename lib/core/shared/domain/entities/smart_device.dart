01b91784448f619e57762b4914af7f1b4eee9944
LionKK020204 <khiemthanhnvx020204@gmail.com>
2025-05-14 01:04:14 +0700

    Add change HTTP to MQTT

--- lib/core/shared/domain/entities/smart_device.dart
+++ lib/core/shared/domain/entities/smart_device.dart
@@ -4,3 +4,18 @@ class SmartDevice {
   late bool isOn;
   final int value;
 }
+// enum DeviceType { light, fan, music, timer }
+//
+// class SmartDevice {
+//   final String name;
+//   final DeviceType type;
+//   bool isOn;
+//   final int? value;
+//
+//   SmartDevice({
+//     required this.name,
+//     required this.type,
+//     required this.isOn,
+//     this.value,
+//   });
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:pbl5_smarthome/core/app/app.dart';
+import 'package:pbl5_smarthome/services/mqtt_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final mqttService = MQTTService(
+    broker: '192.168.1.3',
+    username: 'admin',
+    password: '020204',
+    clientId: 'flutter_app',
+  );
+  await mqttService.connect();
   runApp(const SmartHomeApp());
 }
