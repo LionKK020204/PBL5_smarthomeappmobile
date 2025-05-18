008141ea7bebacb32f5b38b6cc0e010967c06f96
LionKK020204 <khiemthanhnvx020204@gmail.com>
2025-05-18 21:30:18 +0700

    sửa xong nhưng chưa fix mấy phòng không có quạt

--- lib/features/home/presentation/widgets/background_room_lights.dart
+++ lib/features/home/presentation/widgets/background_room_lights.dart
@@ -3,10 +3,12 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:ui_common/ui_common.dart';
 
+import '../../../../controllers/controllerFan.dart';
+import '../../../../controllers/controllerLight.dart';
 import '../../../../core/core.dart';
 import '../../../../globals/globals.dart';
 
-class BackgroundRoomCard extends StatelessWidget {
+class BackgroundRoomCard extends StatefulWidget {
   const BackgroundRoomCard({
     required this.room,
     required this.translation,
@@ -17,22 +19,66 @@ class BackgroundRoomCard extends StatelessWidget {
   final double translation;
 
   @override
+  State<BackgroundRoomCard> createState() => _BackgroundRoomCardState();
+}
+
+class _BackgroundRoomCardState extends State<BackgroundRoomCard> {
+  late bool isLightOn;
+  late bool isFanOn;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isLightOn = widget.room.lights.isOn;
+    isFanOn = widget.room.fanCondition.isOn;
+
+    final lightController = context.read<ControllerLight>();
+    final fanController = context.read<ControllerFan>();
+
+    _listenLightStatus(lightController);
+    _listenFanStatus(fanController);
+  }
+
+  void _listenLightStatus(ControllerLight controller) {
+    controller.listenLightStatus(int.parse(widget.room.id), (status) {
+      if (mounted) {
+        setState(() {
+          isLightOn = status.toUpperCase() == 'ON';
+        });
+      }
+    });
+  }
+
+  void _listenFanStatus(ControllerFan controller) {
+    controller.listenFanStatus(int.parse(widget.room.id), (status) {
+      if (mounted) {
+        setState(() {
+          isFanOn = status.toUpperCase() == 'ON';
+        });
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     final env = context.watch<GlobalEnvironmentData>();
+    final lightController = context.read<ControllerLight>();
+    final fanController = context.read<ControllerFan>();
 
     return Transform(
-      transform: Matrix4.translationValues(0, 80 * translation, 0),
+      transform: Matrix4.translationValues(0, 80 * widget.translation, 0),
       child: DecoratedBox(
         decoration: const BoxDecoration(
           color: SHColors.trackColor,
           borderRadius: BorderRadius.all(Radius.circular(12)),
-          // boxShadow: [
-          //   BoxShadow(
-          //     color: Colors.black26,
-          //     blurRadius: 12,
-          //     offset: Offset(-7, 7),
-          //   ),
-          //],
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 12,
+              offset: Offset(-7, 7),
+            ),
+          ],
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
@@ -63,22 +109,34 @@ class BackgroundRoomCard extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   _DeviceIconSwitcher(
-                    onTap: (value) {},
                     icon: const Icon(SHIcons.lightBulbOutline),
                     label: const Text('Lights'),
-                    value: room.lights.isOn,
+                    value: isLightOn,
+                    onTap: (value) {
+                      setState(() {
+                        isLightOn = value;
+                      });
+                      lightController.toggleLight(value, int.parse(widget.room.id));
+                      lightController.setLightBrightness(int.parse(widget.room.id), value ? 50 : 0);
+                    },
                   ),
                   _DeviceIconSwitcher(
-                    onTap: (value) {},
                     icon: const Icon(SHIcons.fan),
                     label: const Text('Fan'),
-                    value: room.fanCondition.isOn,
+                    value: isFanOn,
+                    onTap: (value) {
+                      setState(() {
+                        isFanOn = value;
+                      });
+                      fanController.toggleFan(value, int.parse(widget.room.id));
+                      fanController.setFanSpeed(int.parse(widget.room.id), value ? 50 : 0);
+                    },
                   ),
                   _DeviceIconSwitcher(
                     onTap: (value) {},
                     icon: const Icon(SHIcons.music),
                     label: const Text('Music'),
-                    value: room.musicInfo.isOn,
+                    value: widget.room.musicInfo.isOn,
                   ),
                 ],
               ),
